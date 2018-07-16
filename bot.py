e2d6bf6784349c93c6f3490ed5de7a655eb4916f
GrigoryTem <41284129+GrigoryTem@users.noreply.github.com>
2018-07-16 23:52:41 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,8 +14,7 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.content.upper().startswith('arina'):
-        userID = message.author.id
-        await client.send_message(message.channel, "<@%s> , I am not mafia maybe YOU!" % (userID))
+    if message.content.upper() == "arina":
+        await client.send_message(message.channel, ":cookie:")
 
 client.run("NDY3NzM0MTc0NjI4MDUzMDI2.Di3fdg.O_hg2m8pEdY5w87L-B6QdN__gDQ") #Replace token with your bots token
