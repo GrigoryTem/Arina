9099be24b1900e92b733d27c021c28cdea318641
GrigoryTem <41284129+GrigoryTem@users.noreply.github.com>
2018-07-16 23:44:18 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,8 +8,6 @@ import time
 Client = discord.Client() #Initialise Client 
 client = commands.Bot(command_prefix = "?") #Initialise client bot
 
-chat_filter = ["PINEAPPLE", "APPLE", "CHROME"]
-
 @client.event 
 async def on_ready():
     print("Bot is online and connected to Discord") #This will be called when the bot connects to the server
@@ -17,6 +15,7 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if message.content.upper().startswith('arina'):
+        userID = message.author.id
         await client.send_message(message.channel, "<@%s> , I am not mafia maybe YOU!" % (userID))
 
 client.run("NDY3NzM0MTc0NjI4MDUzMDI2.Di3fdg.O_hg2m8pEdY5w87L-B6QdN__gDQ") #Replace token with your bots token
