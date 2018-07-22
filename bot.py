94df6b11ca125a90de92fc423b3f099bf57a1db4
GrigoryTem <41284129+GrigoryTem@users.noreply.github.com>
2018-07-22 18:19:10 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext.commands import Bot
 from discord.ext import commands
 import asyncio
+import random
 import time
 
 
@@ -20,5 +21,7 @@ async def on_message(message):
         await client.send_message(message.channel, "mafia - I can choose victim\nmaniac - hm what is victim?")
     if message.content == "mafia":
         await client.send_message(message.channel, "I dont care")
+    if "Voting" in message.content:
+        await client.send_message(message.channel, random.choice["Akbar", "Fox", "Marcus", "Grigory"])
 
 client.run("NDY3NzM0MTc0NjI4MDUzMDI2.Di3fdg.O_hg2m8pEdY5w87L-B6QdN__gDQ") #Replace token with your bots token
