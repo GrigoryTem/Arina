546712cecabd1228f0a5f85915fae2344ea835fd
GrigoryTem <41284129+GrigoryTem@users.noreply.github.com>
2018-07-22 18:46:34 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,7 +18,7 @@ async def on_ready():
 @client.event
 async def on_message(message):
     for word in greetings:
-    if message.content == word:
+    if message.content == word.upper():
         await client.send_message(message.channel, "mafia - I can choose victim\nmaniac - hm what is victim?")
     if message.content == "mafia":
         await client.send_message(message.channel, "I dont care")
